793dee046e9553d9d599009e00d9e442be3ac9bf
NeverBehave <luoxinhao77@gmail.com>
2018-04-25 13:31:43 +0800

    :taco: fix log and add account identification (#30)

--- src/Log.php
+++ src/Log.php
@@ -17,65 +17,81 @@ use Monolog\Handler\StreamHandler;
 use Bramus\Monolog\Formatter\ColoredLineFormatter;
 use metowolf\Bilibili\Curl;
 
-class Log {
+class Log
+{
 
-	protected static $instance;
+    protected static $instance;
 
-	static public function getLogger()
-	{
-		if (! self::$instance) {
-			self::configureInstance();
-		}
-		return self::$instance;
-	}
+    static public function getLogger()
+    {
+        if (!self::$instance) {
+            self::configureInstance();
+        }
+        return self::$instance;
+    }
 
-	protected static function configureInstance()
-	{
-		$logger = new Logger('Bilibili');
-		$handler = new StreamHandler('php://stdout', getenv('APP_DEBUG') == 'true' ? Logger::DEBUG : Logger::INFO);
+    protected static function configureInstance()
+    {
+        $logger = new Logger('Bilibili');
+        $handler = new StreamHandler('php://stdout', getenv('APP_DEBUG') == 'true' ? Logger::DEBUG : Logger::INFO);
         $handler->setFormatter(new ColoredLineFormatter());
         $logger->pushHandler($handler);
 
-		self::$instance = $logger;
-	}
+        self::$instance = $logger;
+    }
 
-	public static function debug($message, array $context = [])
-	{
-		self::getLogger()->addDebug($message, $context);
-		self::callback(Logger::DEBUG ,'DEBUG', $message);
-	}
+    private static function prefix()
+    {
+        if (getenv('APP_MULTIPLE') == 'true') {
+            return '[' . empty($t = getenv('APP_USER_IDENTITY')) ? getenv('APP_USER') : $t . ']';
+        }
+        return '';
+    }
 
-	public static function info($message, array $context = [])
-	{
-		self::getLogger()->addInfo($message, $context);
-		self::callback(Logger::INFO, 'INFO', $message);
-	}
+    public static function debug($message, array $context = [])
+    {
+        $message = self::prefix() . $message;
+        self::getLogger()->addDebug($message, $context);
+        self::callback(Logger::DEBUG, 'DEBUG', $message);
+    }
 
-	public static function notice($message, array $context = [])
-	{
-		self::getLogger()->addNotice($message, $context);
-		self::callback(Logger::NOTICE, 'NOTICE', $message);
-	}
+    public static function info($message, array $context = [])
+    {
+        $message = self::prefix() . $message;
+        self::getLogger()->addInfo($message, $context);
+        self::callback(Logger::INFO, 'INFO', $message);
+    }
 
-	public static function warning($message, array $context = [])
-	{
-		self::getLogger()->addWarning($message, $context);
-		self::callback(Logger::WARNING, 'WARNING', $message);
-	}
+    public static function notice($message, array $context = [])
+    {
+        $message = self::prefix() . $message;
+        self::getLogger()->addNotice($message, $context);
+        self::callback(Logger::NOTICE, 'NOTICE', $message);
+    }
 
-	public static function error($message, array $context = [])
-	{
-		self::getLogger()->addError($message, $context);
-		self::callback(Logger::ERROR, 'ERROR', $message);
-	}
+    public static function warning($message, array $context = [])
+    {
+        $message = self::prefix() . $message;
+        self::getLogger()->addWarning($message, $context);
+        self::callback(Logger::WARNING, 'WARNING', $message);
+    }
 
-	public static function callback($levelId, $level, $message){
-		$callback_level = empty(getenv('APP_CALLBACK_LEVEL')) ? (Logger::ERROR) :intval(getenv('APP_CALLBACK_LEVEL'));
-		if ($levelId >= $callback_level) {
-			$url = str_replace('{level}', $level, getenv('APP_CALLBACK'));
-			$url = str_replace('{message}', urlencode($message), $url);
-			Curl::get($url);
-		}
-	}
+    public static function error($message, array $context = [])
+    {
+        $message = self::prefix() . $message;
+        self::getLogger()->addError($message, $context);
+        self::callback(Logger::ERROR, 'ERROR', $message);
+    }
+
+    public static function callback($levelId, $level, $message)
+    {
+        $callback_level = empty(getenv('APP_CALLBACK_LEVEL')) ? (Logger::ERROR) : intval(getenv('APP_CALLBACK_LEVEL'));
+        if ($levelId >= $callback_level) {
+            $url = str_replace('{account}', self::prefix(), getenv('APP_CALLBACK'));
+            $url = str_replace('{level}', $level, $url);
+            $url = str_replace('{message}', urlencode($message), $url);
+            Curl::get($url);
+        }
+    }
 
 }
